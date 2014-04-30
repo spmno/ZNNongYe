6bb3018953297747e89c50d5a925114b30c6fe07
spmno <sunqingpeng@hotmail.com>
2014-04-30 22:04:50 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZNNongYe::Application.config.secret_key_base = '926ce5eeffaf1ddb7ca8d03641c353fe6aebcfe700df5decd8e9c85a064e12746dca7eb6302c530df6e4d9ece3796a5714463b24a0b00a835f1c4013372f65ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZNNongYe::Application.config.session_store :cookie_store, key: '_ZNNongYe_session'
