e60dd639a074c3160d6751aac65c6b6c4f380a80
spmno <sunqingpeng@hotmail.com>
2014-05-06 22:21:24 +0800

    add main page body.

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,6 +1,6 @@
 class MainPageController < ApplicationController
   def index
-    @agriculture_infos = Agriculture.last(5)
+    @agricultures = Agriculture.last(5)
     @policies = Policy.last(5)
     @products = Product.last(5)
 
